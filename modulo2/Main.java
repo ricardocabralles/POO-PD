6f001c3599086d9bb8c0193f91c2a807698ac4d0
ricardocabralles <ricardo.rcabral@yahoo.com>
2026-02-24 17:38:04 -0300

    Add files via upload

--- modulo2/Main.java
+++ modulo2/Main.java
@@ -1,17 +1,33 @@
+package com.mycompany.app;
+
 public class Main {
     public static void main(String[] args) {
-        // Criando os objetos...
-        Autor autor = new Autor("Jessica Felix", "Brasileira");
-        Livro livro = new Livro("Java for Beginners", autor, "Tecnologia");
+        
+        Autor autor = new Autor("Jessica Felix", "Brasileira", false);
+        
+       
+        Livro livro = new Livro("Java for Beginners", autor, "Tecnologia", true);
+        
+        
         Usuario usuario = new Usuario("Lucas Rafael", 25);
 
-        // Simulando que o livro já foi emprestado para mostrar a mensagem
+       
+        Artigo artigo = new Artigo("Entendendo Compiladores", autor, "tecnologia", true);
+
+        
+        System.out.println("--- Sistema da Biblioteca ---");
+        System.out.println("Artigo Adicionado: " + artigo.getTitulo());
+        System.out.println("Autor do Artigo: " + autor.getNome());
+        System.out.println("É usuário? " + (autor.isEhUsuario() ? "Sim" : "Não"));
+        System.out.println("-----------------------------");
+
+     
         livro.mudarDisponibilidade(); 
 
         if (!livro.eDisponivel()) {
-            System.out.println("O livro não está disponível");
+            System.out.println("O livro não está disponível no momento.");
             
-            // Criamos o objeto de empréstimo para gerar os dados
+            
             Emprestimo emp = new Emprestimo(livro, usuario);
             emp.imprimirResumo();
         }
