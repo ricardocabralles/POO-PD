7a762b159c2f818cfe2903175c55b516cd731807
ricardocabralles <ricardo.rcabral@yahoo.com>
2026-02-24 17:11:29 -0300

    Add files via upload

--- modulo3/AutorTest.java
+++ modulo3/AutorTest.java
@@ -1,13 +1,12 @@
 package com.mycompany.app;
-
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class AutorTest {
     @Test
     public void testAtributosAutor() {
-        Autor autor = new Autor("Jess", "Brasileira");
+        Autor autor = new Autor("Jess", "Brasileira", false);
         assertEquals("Jess", autor.getNome());
-        assertEquals("Brasileira", autor.getNacionalidade());
+        assertFalse(autor.isEhUsuario());
     }
 }
--- modulo3/UsuarioTest.java
+++ modulo3/UsuarioTest.java
@@ -1,5 +1,4 @@
 package com.mycompany.app;
-
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
